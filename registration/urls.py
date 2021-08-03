d98203982557dff17d7cdcc35c658680dbf5b38d
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-08-04 02:14:45 +0530

    carousel done

--- registration/urls.py
+++ registration/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name="index")
+    path('home', views.index, name = "index"),
+    path('login', views.login, name = "login"),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "carousel.html")
+    return render(request, "land.html")
 
+def login(request):
+    return render(request, "login.html")
