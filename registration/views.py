d27c21707e919d1a660d657ba531c0477fe7b72a
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-08-04 11:27:35 +0530

    profile

--- registration/views.py
+++ registration/views.py
@@ -6,3 +6,6 @@ def index(request):
 def login(request):
     return render(request, "login.html")
 
+def profile(request):
+    return render(request, "profile.html")
+
--- static/app.js
+++ static/app.js
@@ -75,4 +75,39 @@ $(document).ready(function(){
 		
 });
 
+//profile page
+
+$(document).ready(function(){
+	// When page loads...:
+	$("div.content div").hide(); // Hide all content
+  
+	/* Check for hashtag in url */
+	if (window.location.hash.length>0) {
+		console.log(window.location.hash);
+		/*find the menu item with this hashtag*/
+		$( ".profnav li a" ).each(function() {
+			if ( $( this ).attr("href") == window.location.hash )
+				$( this ).parent().addClass("current").show(); // Activate page in menu
+		});
+		$(window.location.hash).fadeIn(); // Fade in the active page content
+	}
+	else { /* no hashtag: */
+	  $(".profnav li:first").addClass("current").show(); // Activate first page
+	  $("div.content div:first").show(); // Show first page content
+	}
+
+	// On Click Event (within list-element!)
+	$(".profnav li").click(function() {
+		$(".profnav li").removeClass("current"); // Remove any active class
+		$(this).addClass("current"); // Add "current" class to selected page
+		
+		$("div.content div").hide(); // Hide all content
+
+    // Find the href attribute value to identify the active page:
+		var activePage = $(this).find("a").attr("href"); 
+		$(activePage).fadeIn(); // Fade in the active page
+	}); // end click method
+	
+}); // end $(document).ready method
+
 
