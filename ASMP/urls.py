d7c79516a7fe76d5a7e43c470e19776811bbe254
Bhavit Gupta <bhavitgupta743@gmail.com>
2021-08-06 00:04:52 +0530

    issues resolved

--- ASMP/urls.py
+++ ASMP/urls.py
@@ -24,32 +24,7 @@ from registration import views as registration_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('registration.urls'))
-    path('register/', register_views.register, name='register'),
-    path('profile/', register_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout'),
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='register/password_reset.html'
-         ),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='register/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='register/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='register/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
-         path("send_otp",register_views.send_otp,name="send otp"),
+    path('', include('registration.urls')),
 ]
 
 if settings.DEBUG:
--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,38 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls.conf import include
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('home', views.index, name = "index"),
     path('login', views.login, name = "login"),
-    path('profile', views.profile, name="profile")
+    path('profile', views.profile, name="profile"),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('logn', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='register/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='register/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='register/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='register/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+         path("send_otp",views.send_otp,name="send otp"),
 ]
