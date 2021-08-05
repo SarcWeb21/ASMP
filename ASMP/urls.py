18a31d8950b9a6d01ff6c8bb269e09e348a4bd3f
captain-harsh <84686348+captain-harsh@users.noreply.github.com>
2021-08-05 20:26:20 +0530

    Update urls.py

--- ASMP/urls.py
+++ ASMP/urls.py
@@ -14,10 +14,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from registration import views as registration_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registration.urls'))
+    path('register/', register_views.register, name='register'),
+    path('profile/', register_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='register/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='register/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='register/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='register/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+         path("send_otp",register_views.send_otp,name="send otp"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
