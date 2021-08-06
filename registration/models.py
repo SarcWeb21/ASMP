876fc730d2bc54028864ff70add5f4be4d256e14
Manav Doshi <200100094@iitb.ac.in>
2021-08-06 10:20:34 +0530

    Changing login functions

--- registration/models.py
+++ registration/models.py
@@ -5,19 +5,25 @@ from PIL import Image
 # Create your models here.
 
 #For profile which includes photo and username 
+#class Profile(models.Model):
+#   user = models.OneToOneField(User, on_delete=models.CASCADE)
+#   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+  
+    #def __str__(self):
+        #return f'{self.user.username} Profile'
+
+    #def save(self):
+        #super().save()
+
+        #img = Image.open(self.image.path)
+
+        #if img.height > 300 or img.width > 300:
+        #   output_size = (300, 300)
+        #   img.thumbnail(output_size)
+        #   img.save(self.image.path)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    
-    def __str__(self):
-        return f'{self.user.username} Profile'
+    otp = models.CharField(max_length=6)
+    password = models.CharField(max_length=20)
 
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
--- registration/views.py
+++ registration/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+#from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.core.mail import send_mail
 import math, random
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, "land.html")
@@ -15,57 +16,94 @@ def login(request):
 def profile(request):
     return render(request, "profile.html")
 
-def register(request):
+def login(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'register/register.html', {'form': form})
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.filter(email=email).first()
+        requested_profile = Profile.objects.filter(user=user).first()
+        if user is None:
+            context = {'message': 'User not found', 'class': 'danger'}
+            return render(request, 'login.html', context)
 
-
-def generateOTP() :
-     digits = "0123456789"
-     OTP = ""
-     for i in range(4) :
-         OTP += digits[math.floor(random.random() * 10)]
-     return OTP
-
-
-
-def send_otp(request):
-     email=request.GET.get("email")
-     print(email)
-     o=generateOTP()
-     htmlgen = '<p>Your OTP is <strong>o</strong></p>'
-     send_mail('OTP request',o,'<your gmail id>',[email], fail_silently=False, html_message=htmlgen)
-     return HttpResponse(o)
-
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.register)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.register.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account has been updated!')
+        if password == requested_profile.password:
+            context = {'email': email}
             return redirect('profile')
 
-    else:
-        u_form = UserUpdateForm(instance=request.register)
-        p_form = ProfileUpdateForm(instance=request.register.profile)
+        return redirect('login')
+    return render(request, 'login.html')
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        password = request.POST.get('password')
+        check_user = User.objects.filter(email=email).first()
+        check_profile = Profile.objects.filter(mobile=mobile).first()
+        # if not email.split('@')[1]=='iitb.ac.in':
+        #     context = {'message': 'Login using your ldap id', 'class':'danger'}
+        #     return render(request, 'register.html', context)
+        if check_user or check_profile:
+            context = {'message': 'User already exists', 'class': 'danger'}
+            return render(request, 'register.html', context)
 
-    return render(request, 'register/profile.html', context)
+        #user = User(email=email, username=name)
+        #user.save()
+        #otp = str(random.randint(1000, 9999))
+        #profile = Profile(user=user, mobile=mobile, otp=otp)
+        #profile.save()
+        #email_success, mobile_success = send_otp(email, mobile, otp)
+        request.session['mobile'] = mobile
+        request.session['email'] = email
+        request.session['otp'] = otp
+        request.session['name'] = name
+        request.session['password'] = password
+        #if not email_success and not mobile_success:
+            #context = {'message': 'OTP failed to generate', 'class': 'danger'}
+            #return render(request, 'regiser.html', context)
+        #return redirect('otp')
+    return render(request, 'register.html')
+
+
+# def generateOTP():
+#     digits = "0123456789"
+#     OTP = ""
+#     for i in range(4) :
+#         OTP += digits[math.floor(random.random() * 10)]
+#     return OTP
+
+
+
+# def send_otp(request):
+#      email=request.GET.get("email")
+#      print(email)
+#      o=generateOTP()
+#      htmlgen = '<p>Your OTP is <strong>o</strong></p>'
+#      send_mail('OTP request',o,'<your gmail id>',[email], fail_silently=False, html_message=htmlgen)
+#      return HttpResponse(o)
+
+
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         u_form = UserUpdateForm(request.POST, instance=request.register)
+#         p_form = ProfileUpdateForm(request.POST,
+#                                    request.FILES,
+#                                    instance=request.register.profile)
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#             messages.success(request, f'Your account has been updated!')
+#             return redirect('profile')
+
+#     else:
+#         u_form = UserUpdateForm(instance=request.register)
+#         p_form = ProfileUpdateForm(instance=request.register.profile)
+
+#     context = {
+#         'u_form': u_form,
+#         'p_form': p_form
+#     }
+
+#     return render(request, 'register/profile.html', context)
